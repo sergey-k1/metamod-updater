0568681853f69fc277353d88fe6c2eecc20bc662
Simone <chaufnet@gmail.com>
2014-10-10 15:28:32 +0200

    First commit.
    This should work.

---
+++ metamod-updater.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+SPATH=$1
+if [ ! -f $SPATH/steam.inf ]; then
+	echo "Usage: $0 path/to/tf2/tf"
+	exit 0
+fi
+mkdir -p $SPATH/.metamod-downloads/
+
+MM1="curl http://www.metamodsource.net/mmsdrop/1.11/"
+MM2=$(curl --silent http://www.metamodsource.net/mmsdrop/1.11/ | egrep -o "mm.*linux.tar.gz\"" | tail -n 1 | head -c -2)
+if [ -f $SPATH/.metamod-downloads/$MM2 ];then
+	echo "MetaMod up to date, skipping."
+	exit 0
+fi
+
+echo "Downloading $MM2"
+rm -rf $PATH/.metamod-downloads/*
+wget -q -O $SPATH/.metamod-downloads/$MM2 $MM1$MM2
+
+tar -zxf $SPATH/.metamod-downloads/$MM2 -C $SPATH
+echo "Installed Metamod ($MM2)"
+echo "Quitting."
+exit 0
