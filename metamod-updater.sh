692ad9fad80e50672d86f652fee1738b4254b27e
Sergey K <mymail333g@gmail.com>
2017-09-08 17:03:58 +0300

    Now can save file metamod.vdf when update metamod

--- metamod-updater.sh
+++ metamod-updater.sh
@@ -17,7 +17,14 @@ echo "Downloading $MM2"
 rm -rf $PATH/.metamod-downloads/*
 wget -q -O $SPATH/.metamod-downloads/$MM2 $MM1$MM2
 
+if [ -f $SPATH/addons/metamod.vdf ]; then
+	cp $SPATH/addons/metamod.vdf $SPATH/addons/metamod.vdf_back
+fi
+
 tar -zxf $SPATH/.metamod-downloads/$MM2 -C $SPATH
+if [ -f $SPATH/addons/metamod.vdf_back ]; then
+	mv $SPATH/addons/metamod.vdf_back $SPATH/addons/metamod.vdf
+fi
 echo "Installed Metamod ($MM2)"
 echo "Quitting."
 exit 0
